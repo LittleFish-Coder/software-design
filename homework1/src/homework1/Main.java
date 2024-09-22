c9ee38902fcdcdca223972650b4f92fef69e387e
LittleFish-Coder <adrianyu893017@gmail.com>
2024-09-22 16:54:17 +0800

    update hw1

--- homework1/src/homework1/Main.java
+++ homework1/src/homework1/Main.java
@@ -2,11 +2,12 @@ package homework1;
 
 public class Main {
 	public static void main(String[] args) {
-		Animal myDog = new Dog();
-		Animal myCat = new Cat();
-		Animal mySheep = new Sheep();
+//		Animal myDog = new Dog();
+//		Animal myCat = new Cat();
+//		Animal mySheep = new Sheep();
+//		Animal[] animals = {myDog, myCat, mySheep};
 		
-		Animal[] animals = {myDog, myCat, mySheep};
+		Animal[] animals = {new Dog(), new Cat(), new Sheep()};
 		
 		// iterate the animal list
 		for(Animal animal: animals) {
