72cc28bd76f3e24300e802b4b6362703554eec3c
LittleFish-Coder <adrianyu893017@gmail.com>
2024-10-21 22:38:51 +0800

    change name

---
+++ lab1/src/lab1/CreditCardPayment.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class CreditCardPayment extends Payment {
+	public void pay() {
+		System.out.println("Pay with Credit card");
+	}
+}
---
+++ lab1/src/lab1/Payment.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Payment {
+	public void pay() {
+		System.out.println("Pay in cash");
+	}
+	
+	public void checkout() {
+		pay();
+	}
+}
